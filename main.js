d5d7c4ab13b1a68c17ba93a686aeced8a849619b
Jorge F Diaz <jorgefederico24@hotmail.com>
2023-06-04 23:02:18 -0400

    ultimocomit

--- main.js
+++
@@ -1,7 +0,0 @@
-ScrollReveal().reveal(".hero");
-ScrollReveal().reveal(".hero", { delay: 1000 });
-ScrollReveal().reveal(".container", { delay: 100 });
-
-var updates = 0;
-
-var insertedNode = parentNode.insertBefore(newNode, referenceNode);
